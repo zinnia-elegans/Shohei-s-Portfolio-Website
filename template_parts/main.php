0def4d4ba45cf9d288d7078ef12c457f61cf70f6
zinnia-elegans <sono.genoano@gmail.com>
2021-01-03 18:38:59 +0900

    初回コミット

--- template_parts/main.php
+++ template_parts/main.php
@@ -1,6 +1,6 @@
 <!-- main -->
 <section class="main main-inner">
-    <div class="main-content">
+    <div class="main-content wow fadeIn">
         <h1 class="main-text"><?php the_title(); ?></h1>
     </div>
 </section>
